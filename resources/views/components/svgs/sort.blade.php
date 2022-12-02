2f357338a56292eea0f2ec22808eeba25c8dd175
datomaluta <datomaluta@gmail.com>
2022-12-02 14:51:13 +0400

    fix: use named route for queries

--- resources/views/components/svgs/sort.blade.php
+++ resources/views/components/svgs/sort.blade.php
@@ -1,4 +1,5 @@
+@props(['query'])
 <svg width="16" height="24" viewBox="0 0 16 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M8 18.5L3 13.5H13L8 18.5Z" fill="#010414" />
-    <path d="M8 5.5L13 10.5L3 10.5L8 5.5Z" fill="#BFC0C4" />
+    <path  d="M8 18.5L3 13.5H13L8 18.5Z" fill="{{ request($query) == 'asc' ? '#010414' : '#BFC0C4' }}" />
+    <path d="M8 5.5L13 10.5L3 10.5L8 5.5Z" fill="{{ request($query) == 'desc' ? '#010414' : '#BFC0C4' }}" />
 </svg>
--- resources/views/dashboard/bycountry/index.blade.php
+++ resources/views/dashboard/bycountry/index.blade.php
@@ -10,22 +10,22 @@
     <div
         class="h-[38rem] mt-10 sm:mt-6  rounded-lg overflow-hidden shadow-lg pb-16 mb-14 text-sm sm:absolute sm:left-0 sm:right-0 ">
         <div class="flex bg-neutral-100 py-5 pl-10 lg:pl-2 pr-[3rem] lg:pr-0 sm:pl-0">
-            <a href="?location={{ request('location') == 'desc' ? 'asc' : 'desc' }}{{ request('search') ? '&search=' . request('search') : '' }}"
+            <a href="{{ route('dashboard.country', ['location' => request('location') == 'desc' ? 'asc' : 'desc','search'=>request('search')]) }}"
                 class="flex items-center w-1/4 font-semibold">{{ __('dashboard.location') }}
-                <x-svgs.sort />
+                <x-svgs.sort query="location" />
             </a>
-            <a href="?confirmed={{ request('confirmed') == 'desc' ? 'asc' : 'desc' }}{{ request('search') ? '&search=' . request('search') : '' }}"
+            <a href="{{ route('dashboard.country', ['confirmed' => request('confirmed') == 'desc' ? 'asc' : 'desc','search'=>request('search')]) }}"
                 class="flex items-center  w-1/4 font-semibold">{{ __('dashboard.new_cases') }}
-                <x-svgs.sort />
+                <x-svgs.sort query="confirmed"/>
             </a>
-            <a href="?deaths={{ request('deaths') == 'desc' ? 'asc' : 'desc' }}{{ request('search') ? '&search=' . request('search') : '' }}"
+            <a href="{{ route('dashboard.country', ['deaths' => request('deaths') == 'desc' ? 'asc' : 'desc','search'=>request('search')]) }}"
                 class="flex items-center gap-1 sm:gap-0 sm:break-all w-1/4 sm:justify-center font-semibold">
                 {{ __('dashboard.deaths') }}
-                <x-svgs.sort />
+                <x-svgs.sort query="deaths"/>
             </a>
-            <a href="?recovered={{ request('recovered') == 'desc' ? 'asc' : 'desc' }}{{ request('search') ? '&search=' . request('search') : '' }}"
+            <a  href="{{ route('dashboard.country', ['recovered' => request('recovered') == 'desc' ? 'asc' : 'desc','search'=>request('search')]) }}"
                 class="flex items-center gap-1 sm:gap-0 sm:break-all w-1/4 font-semibold">{{ __('dashboard.recovered') }}
-                <x-svgs.sort />
+                <x-svgs.sort query="recovered"/>
             </a>
         </div>
 
